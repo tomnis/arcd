29d0f42c2d7970a0f0e41a70c733b19763f49e6c
tomas.mccandless <tomas.mccandless@workday.com>
2018-06-28 16:46:09 -0700

    minor fixes

--- src/test/scala/org/mccandless/arcd/ArcDiagramSpec.scala
+++ src/test/scala/org/mccandless/arcd/ArcDiagramSpec.scala
@@ -10,27 +10,38 @@ import org.pmw.tinylog.Logger
   */
 class ArcDiagramSpec extends BaseSpec {
 
+  private def logLongArcs(s: String, arcs: Seq[Arc], width: Int): Unit = {
+    arcs filter { _.width > width } foreach { a =>
+      Logger.info(s"$a, ${s.substring(a.start1, a.start1 + a.width)}  ${s.substring(a.start2, a.start2 + a.width)}")
+    }
+  }
+
   @Test
   def diagram(): Unit = {
     val s = "abcjdskfhsabcjdsha"
     val arcs: Seq[Arc] = s.getArcs
 
-    arcs should contain (Arc(0, 10, 6))
-    arcs filter { _.width > 4 } foreach { a =>
-      Logger.info(s"$a, ${s.substring(a.start1, a.start1 + a.width)}  ${s.substring(a.start2, a.start2 + a.width)}")
-    }
+    arcs should contain(Arc(0, 10, 6))
+    logLongArcs(s, arcs, 4)
   }
 
 
   @Test
   def exampleFromPaper(): Unit = {
-    val s = "28314159479735648231415937"
+    val s = "28746391479735648274639137"
     val arcs: Seq[Arc] = s.getArcs
-    arcs filter { _.width > 4 } foreach { a =>
-      Logger.info(s"$a: left arc substring: ${s.substring(a.start1, a.start1 + a.width)} right arc substring: ${s.substring(a.start2, a.start2 + a.width)}")
-    }
+    logLongArcs(s, arcs, 4)
     arcs should contain (Arc(2, 18, 6))
-    s.substring(2, 8) should be ("314159")
-    s.substring(18, 24) should be ("314159")
+    s.substring(2, 8) should be ("746391")
+    s.substring(18, 24) should be ("746391")
+  }
+
+
+  @Test
+  def repeatedExample(): Unit = {
+    val s = "1234567abcde1234567fghij1234567"
+
+    val arcs: Seq[Arc] = s.getArcs
+    logLongArcs(s, arcs, 6)
   }
 }
